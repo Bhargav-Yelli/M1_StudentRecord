0fa91286f27992fd932fd6ae9921482a0a2058f2
Bhargav-Yelli <101517291+Bhargav-Yelli@users.noreply.github.com>
2022-04-02 19:42:56 +0530

    Add files via upload

---
+++ 3_Implementation/inc/methodfunction.h
@@ -0,0 +1,29 @@
+#ifndef METHODFUNCTIONS_H_INCLUDED
+#define METHODFUNCTIONS_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <windows.h>
+
+void SetColor();
+//void ClearConsoleToColors();
+void SetColorAndBackground();
+void drawRectangle();
+void gotoxy();
+void clearWindow();
+//void window();
+void get_password();
+void use_pass_field();
+void print_heading();
+int conf_record();
+void add_student();
+void search_student();
+void mod_student();
+void gen_marksheet();
+void delete_student();
+//void main_window();
+//void WinMain();
+
+#endif
---
+++ 3_Implementation/inc/user.h
@@ -0,0 +1,14 @@
+#ifndef USER_H_INCLUDED
+#define USER_H_INCLUDED
+
+struct student{
+    char ID[15];
+    char name[20];
+    char add[20];
+    char parname[20];
+    int Class;
+    long unsigned int phone_no;
+};
+
+
+#endif 
