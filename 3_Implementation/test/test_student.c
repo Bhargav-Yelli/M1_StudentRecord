57027b5a2e1d45614721a35a17e854f949d20a3b
Bhargav-Yelli <101517291+Bhargav-Yelli@users.noreply.github.com>
2022-04-02 20:12:29 +0530

    Create test_student.c

---
+++ 3_Implementation/test/test_student.c
@@ -0,0 +1,38 @@
+#include "unity.h"
+
+#include "methodfunction.h"
+#include "user.h"
+
+
+
+
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+
+
+
+/* Prototypes for all the test functions */
+
+
+void test_unity(void){
+        
+       
+        TEST_ASSERT_EQUAL(NULL,main());
+        
+      
+}
+
+
+
+
+int main(void)
+{
+UNITY_BEGIN();
+RUN_TEST(test_unity);
+
+return UNITY_END();
+}
